12f5a1cd2203ffd6dab72c2e3a03d67b3e45ab17
Werewolf <lawsonba1@s.dcsdk12.org>
2021-02-17 11:03:47 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/whackaworm/LevelOne.java
@@ -0,0 +1,14 @@
+package com.example.whackaworm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_one);
+    }
+}
