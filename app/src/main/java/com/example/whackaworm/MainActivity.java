3c199bb1270e5c6ec9dd76553b6fa752a38eb9df
Werewolf <lawsonba1@s.dcsdk12.org>
2021-02-17 11:27:08 -0700

    Initial Commit

--- app/src/main/java/com/example/whackaworm/MainActivity.java
+++ app/src/main/java/com/example/whackaworm/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.whackaworm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageViewPlay = (ImageView) findViewById(R.id.playImageView);
+        imageViewPlay.setOnTouchListener(new View.OnTouchListener() {
+
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                Log.i("imageviewandontouchlistener", "playImageView onTouch");
+                return false;
+            }
+        });
     }
-}
